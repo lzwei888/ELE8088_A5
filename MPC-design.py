0bd4bb3018cfbda7f909c2b699eaee1dd4c9178f
lzwei888 <l18519655819@gmail.com>
2024-02-11 11:57:07 +0000

    MPC design

--- MPC-design.py
+++ MPC-design.py
@@ -3,7 +3,6 @@ import pandas as pd
 import control as ctrl
 import polytope as pc
 import numpy as np
-import control.optimal as obc
 import matplotlib.pyplot as plt
 import scipy.linalg as spla
 from qpsolvers import solve_qp
@@ -15,19 +14,21 @@ A = np.array([[0.983500, 2.782, 0],
 B = np.array([[0.01293],
               [0.00100],
               [0.001425]])
+
 # Assuming all states are measurable
-# Q = np.eye(3)
 Q = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 # Assuming there is no direct path through
 R = np.array([1])
 
-# Convert the model to a discrete-time system with a sampling time of 0.05 seconds
+# sampling time of 0.05 seconds
 dt = 0.05
 
+# get matrix P_f
 P, _, K = ctrl.dare(A, B, Q, R)
 K = -K
 Acl = A + B @ K
 
+# set lb and ub, change deg to rad
 x_min = np.array([[-0.2007], [-0.2443], [-0.6109]])
 x_max = -x_min
 u_min = np.array([-0.4189])
@@ -79,8 +80,11 @@ b_1_before = np.concatenate((b_x, b_u, np.reshape(b_infty, (-1, 1))))
 S_1_before = pc.Polytope(H_1_before, b_1_before)
 X_1 = S_1_before.project([1, 2])
 
-# n = 20
+# set matrix G_z
 
+# the code following code is unnecessary, it just shows the process of trying to create G
+# the matrix is imported into Excel, and it is easier to modify it in Excel or in matlab to increase N.
+# python can use pandas to read excel to get the matrix.
 g1 = np.append(1, np.zeros((1, 4)))
 g2 = np.append(-1, np.zeros((1, 4)))
 g3 = np.append(np.array([0, 1]), np.zeros((1, 3)))
@@ -103,11 +107,13 @@ G1 = np.vstack((gg1, gg2, gg3, gg4, np.zeros((8, 20))))
 G2 = np.vstack((np.zeros((40, 4)),
                 np.array([[1, 0, 0, 0], [-1, 0, 0, 0], [0, 1, 0, 0], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, -1, 0],
                           [-1, 0, 1, 0], [1, 0, -1, 0]])))
-G = np.hstack((G1, G2))
+G = np.hstack((G1, G2))  # G
 
+# create h
 h = np.array([[0.2007], [0.2007], [0.2443], [0.2443], [0.6109], [0.6109], [0.4014], [0.4014], [0.4712], [0.4189]])
 h = np.vstack((h, h, h, h, np.array([[0.2007], [0.2007], [0.2443], [0.2443], [0.6109], [0.6109], [0.4014], [0.4014]])))
 
+# create A_z
 Azz = np.vstack(((np.hstack((A, np.zeros((3, 1)), B, np.array([[-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0]]))),
                   (np.array([-1, 0, 1, -1, 0, 0, 0, 0, -1])))))
 Az1 = np.hstack((Azz, np.zeros((4, 15))))
@@ -116,14 +122,18 @@ Az3 = np.hstack((np.zeros((4, 10)), Azz, np.zeros((4, 5))))
 Az4 = np.hstack((np.zeros((4, 15)), Azz))
 
 Az = np.vstack((Az1, Az2, Az3, Az4))
+
+# create b_z
 # initial state
 bz = np.vstack((np.zeros((12, 1)), np.array([[-0.2], [0.1], [0.1], [-0.1]])))
 
+# set lb and ub, delta is included
 zmin = np.array([[-0.2007], [-0.2443], [-0.6109], [-0.4014], [-0.4189]])
 zmax = np.array([[0.2007], [0.2443], [0.6109], [0.4014], [0.4712]])
 z_min = np.vstack((zmin, zmin, zmin, zmin, np.array([[-0.2007], [-0.2443], [-0.6109], [-0.4014]])))
 z_max = np.vstack((zmax, zmax, zmax, zmax, np.array([[0.2007], [0.2443], [0.6109], [0.4712]])))
 
+# get new P_f after adding delta
 A = np.array([[0.983500, 2.782, 0, 0],
               [-0.0006821, 0.978, 0, 0],
               [-0.0009730, 2.804, 1, 0],
@@ -135,64 +145,26 @@ B = np.array([[0.01293],
 
 Q = np.eye(4)
 R = np.array([1])
-Pf, _, K = ctrl.dare(A, B, Q, R)
+Pf, _, K = ctrl.dare(A, B, Q, R)    # P_f
 
+# create P
 P1 = np.hstack((np.eye(5), np.zeros((5, 19))))
 P2 = np.hstack((np.zeros((5, 5)), np.eye(5), np.zeros((5, 14))))
 P3 = np.hstack((np.zeros((5, 10)), np.eye(5), np.zeros((5, 9))))
 P4 = np.hstack((np.zeros((5, 15)), np.eye(5), np.zeros((5, 4))))
 P5 = np.hstack((np.zeros((4, 20)), Pf))
 P = np.vstack((P1, P2, P3, P4, P5))
-
+# create q
 q = np.zeros((24, 1))
 
-start=time.time()
+# solve and record time
+start = time.time()
 x = solve_qp(P, q, G, h, Az, bz, lb=z_min, ub=z_max, solver="proxqp")
-end=time.time()
+end = time.time()
 print(f"QP solution: {x = }")
-print("time: ", start-end)
+print("time: ", start - end)
 
 # data_df = pd.DataFrame(Az)
-# writer = pd.ExcelWriter('A.xlsx')  # 关键2，创建名称为hhh的excel表格
-# data_df.to_excel(writer, 'page_1', float_format='%.5f')  # 关键3，float_format 控制精度，将data_df写到hhh表格的第一页中。若多个文件，可以在page_2中写入
+# writer = pd.ExcelWriter('A.xlsx')
+# data_df.to_excel(writer, 'page_1', float_format='%.5f')
 # writer.save()
-
-
-'''
-# lb=z_min, ub=z_max,
-Mz = np.array([[1.0, 2.0, 0.0], [0.0, 2.0, 3.0], [0.0, 0.0, 1.0]])
-Pzz = Mz.T @ Mz
-QR = np.vstack((np.hstack((Pzz, np.zeros((3, 1)))), np.array([0, 0, 0, 1])))
-
-# QR = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 2, 0], [0, 0, 0, 1]])
-
-I = np.eye(3)
-daleth = np.vstack((np.hstack((A, B, -I, np.zeros((3, 8)))), np.hstack((np.zeros((3, 4)), A, B, -I, np.zeros((3, 4)))),
-                    np.hstack((np.zeros((3, 8)), A, B, -I)), np.hstack((I, np.zeros((3, 12))))))
-eta = np.vstack((np.zeros((11, 1)), 1))
-G = np.hstack((np.vstack((H_1_before, np.zeros((4, 4)))), np.zeros((140, 11))))
-h = np.vstack((b_1_before, np.zeros((4, 1))))
-# Pz = np.vstack((np.hstack((np.eye(12), np.zeros((12, 3)))), np.hstack((np.zeros((3, 12)), P))))
-Pz = np.vstack((np.hstack((QR, np.zeros((4, 11)))), np.hstack((np.zeros((4, 4)), QR, np.zeros((4, 7)))),
-                np.hstack((np.zeros((4, 8)), QR, np.zeros((4, 3)))), np.hstack((np.zeros((3, 12)), P))))
-# qz = np.ones((15, 1))
-qz = np.vstack((np.zeros((14, 1)), 1))
-zmin1 = np.vstack((x_min, u_min))
-zmin = np.vstack((zmin1, zmin1, zmin1, x_min))
-zmax1 = np.vstack((x_max, u_max))
-zmax = np.vstack((zmax1, zmax1, zmax1, x_max))
-
-
-def is_pos_def(x):
-    return np.all(np.linalg.eigvals(x) > 0)
-
-
-if is_pos_def(Pz):
-    print("yes")
-    x = solve_qp(Pz, qz, G, h, daleth, eta, lb=zmin, ub=zmax, solver="proxqp")
-    print(f"QP solution: {x = }")
-else:
-    print("Pz false")
-
-# Pz = np.vstack((np.hstack((np.zeros((3, 137)), P)), np.hstack((np.eye(137), np.zeros((137, 3))))))
-# qz = np.ones((140,1))'''
